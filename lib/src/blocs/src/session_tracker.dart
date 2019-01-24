23e2eb8d53a12a03aaa18d3a93cb94cd387b91ea
Max Onishi <max@bluechilli.com>
2019-01-24 12:39:53 +1100

    refactor

--- lib/src/blocs/src/session_tracker.dart
+++ lib/src/blocs/src/session_tracker.dart
@@ -7,6 +7,8 @@ class SessionTracker {
   ValueObservable<Session> get session => _sessionController.stream;
   void sessionLoaded(Session session) => _sessionController.add(session);
   void sessionEnded() => _sessionController.add(null);
+  bool get isAuthenticated =>
+      this.session.value != null && this.session.value?.userKey != null;
 
   void dispose() {
     _sessionController.close();
